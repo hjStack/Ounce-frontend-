a1d480bb95c0f926b51ebf6ced09490b9405b449
hjStack <minnie1498@naver.com>
2025-11-04 19:48:50 +0900

    start

---
+++ Desktop/nhnacademy_8/src/test/java/com/example/nhnacademy_8/Nhnacademy8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nhnacademy_8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nhnacademy8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
