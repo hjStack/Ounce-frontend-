60f176cb02dc73da805f6fa4dfda385d7bf3b738
hjStack <minnie1498@naver.com>
2025-12-05 11:22:05 +0900

    feat:test

---
+++ Desktop/high-five/src/main/java/com/example/high_five/HighFiveApplication.java
@@ -0,0 +1,13 @@
+package com.example.high_five;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighFiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HighFiveApplication.class, args);
+	}
+
+}
