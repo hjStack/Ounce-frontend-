1e18b5da8d65daad5ea9483a4887a1ad27bcc442
hjStack <minnie1498@naver.com>
2026-08-29 03:57:39 +0900

    Feat:s3

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // 💡 여기에 S3 이미지 도메인 허용 설정을 추가합니다!
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ounce-bucket-185271206377-ap-northeast-2-an.s3.ap-northeast-2.amazonaws.com",
+      },
+    ],
+  },
 
   output: "standalone",
-  
+
   turbopack: {
     root: process.cwd(),
   },
+  
   async rewrites() {
     const backend = process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:8081";
 
